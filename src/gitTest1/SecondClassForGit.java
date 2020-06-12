61956bcfed14eae4bd13c16cbcf678f0dfaf7715
Mario Briseno <66706205+mariob4113@users.noreply.github.com>
2020-06-11 18:40:20 -0700

    this is the second class for the prokect that we are creating in eclipse for git huhb

---
+++ src/gitTest1/SecondClassForGit.java
@@ -0,0 +1,8 @@
+package gitTest1;
+
+public class SecondClassForGit {
+public static void main(String[] args) {
+	System.out.println("hey this is just a test of your regularly scheduled programing");
+	System.out.println("this is also a test please disreguard thanksss");
+}
+}
